578891bea717fc3969db6b6af051ea0e1329874c
Antonio Emilio <antonio_pereira@outlook.com>
2020-09-05 19:12:30 -0300

    Primeiro commit com um hello world inicial.

---
+++ src/ifound.c
@@ -0,0 +1,16 @@
+/*
+ ============================================================================
+ Nome dos integrantes: MELYSSA MARIANA GOMES SILVA E ANTONIO EMILIO PEREIRA
+ Nome do projeto: iFound
+ Versão: 1.0
+ Descrição: Software para organização de estoque de componentes eletrônicos
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
